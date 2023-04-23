6675713fa5b34e14c81c58b78c479f162b92a88d
Bruno Pereyra <brunopereyra459@gmail.com>
2023-04-23 01:13:22 -0300

    railway6

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"backend/config"
+	"backend/database"
+	"backend/models"
 	"backend/routes"
+	"context"
+	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func main() {
@@ -18,8 +24,24 @@ func main() {
 
 	routes.UseRoutes(app)
 	app.Get("/home", func(c *fiber.Ctx) error {
-		config.CLOUDINARY_URL()
-		return c.SendString(config.CLOUDINARY_URL())
+		db, err := database.GoMongoDB()
+		if err != nil {
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"message": "StatusServiceUnavailable",
+			})
+		}
+		collection := db.Collection("users")
+
+		// traer primeros campeonatos si esta vacio el el req
+		var user models.User
+		datafind, _ := primitive.ObjectIDFromHex("6441dee7fa046c203a30b030")
+		errfindChampions := collection.FindOne(context.TODO(), bson.D{{Key: "_id", Value: datafind}}).Decode(&user)
+		if errfindChampions != nil {
+			fmt.Println("errfindChampions")
+		}
+		return c.JSON(fiber.Map{
+			"data": user,
+		})
 	})
 
 	if PORT == "" {
